13fe9483f5dd3c31b55087c022f84b226538a3a4
tomohiro <tomohiro@MacBookProT.local>
2022-06-26 10:36:44 +0900

    Add: Active, InActive, Backgroundの切り替わりを判断する実装を追加

--- CountryPOS/CountryPOS/ContentView.swift
+++ CountryPOS/CountryPOS/ContentView.swift
@@ -8,9 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.scenePhase) var scenePhase
     var body: some View {
         Text("Hello, world!")
             .padding()
+            .onChange(of: scenePhase) { newPhase in
+                if newPhase == .active {
+                    print("Active")
+                } else if newPhase == .inactive {
+                    print("Inactive")
+                } else if newPhase == .background {
+                    print("Background")
+                }
+            }
     }
 }
 
